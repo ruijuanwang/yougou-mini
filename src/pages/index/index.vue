84c3b23318352272c2bed2ac41b8e228b7f4c628
ruijuanwang <1938608827@qq.com>
2020-04-10 19:06:57 +0800

    首页-搜索部分

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,17 @@
 <template>
+<!-- 首页 -->
 	<view class="content">
+		<!-- 搜索 -->
+		<view class="search">
+			<!-- 搜索框 -->
+			<view class="search-input">
+				<input type="text" placeholder="请输入要搜索的商品" >
+			</view>
+			<!-- 搜索内容 -->
+			<view class="search-content">
+			
+			</view>
+		</view>
 	
 	</view>
 </template>
@@ -10,6 +22,18 @@
 	}
 </script>
 
+
 <style>
-	
+.search .search-input{
+	background-color: #ea4451;
+	padding: 20rpx 15rpx;
+}
+.search .search-input input{
+	background-color: #fff;
+	height: 60rpx;
+	border-radius: 5rpx;
+	font-size: 28rpx;
+	color: #ccc;
+	padding-left: 10rpx;
+}	
 </style>
