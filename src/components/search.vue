9bdcbc269cb70014b95f53e58e6966ef54dda9fe
ruijuanwang <1938608827@qq.com>
2020-04-11 20:25:54 +0800

    搜索组件-历史搜索占满屏幕

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,12 +1,12 @@
 <template>
-  <!-- 搜索组件 -->
-  <view class="search active">
+  <!-- 搜索组件  动态绑定类名 获取焦点显示 active 的样式-->
+  <view class="search" :class="{active:isfocus}">
 			<!-- 搜索框 -->
 			<view class="search-input">
-				<input type="text" placeholder="请输入要搜索的商品" >
-				<text class="cancel">取消</text>
+				<input @focus="goSearch" type="text" placeholder="请输入要搜索的商品" >
+				<text @click="goCancel" class="cancel">取消</text>
 			</view>
-			<!-- 搜索内容 -->
+			<!-- 搜索内容  显示的时候要沾满屏幕-->
 			<view class="search-content">
 			
 			</view>
@@ -15,6 +15,22 @@
 
 <script>
 export default {
+	data(){
+		return{
+			isfocus:false // 默认不显示类名
+		}
+	},
+	methods:{
+		// 获取焦点的方法
+		goSearch(){
+			this.isfocus=true // 显示类名
+
+		},
+		// 点击取消 
+		goCancel(){
+			this.isfocus=false //隐藏active 
+		}
+	}
 
 }
 </script>
@@ -45,21 +61,39 @@ export default {
 
 	}
    
-	// .search.active 交集选择器 控制获取焦点样式
-	&.active{
-		.search-input{
-			display: flex;
-			background-color: #eee; // 背景色变成灰色
-			input{
-				flex:1;		
-			}
+// .search.active 交集选择器 控制获取焦点样式  获取焦点 才显示这些样式
+&.active{
+	 // 大盒子占满屏幕
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index:999;
+    background-color: pink;
+	.search-input{
+		display: flex;
+		background-color: #eee; // 背景色变成灰色
+		input{
+			flex:1;		
+		}
 			// 获取焦点 时取消的样式
-			.cancel{
+		.cancel{
 				display: block;
 
 		}
-			}
-	}
+		}
+	// 搜索历史内容部分占满剩下的 
+    .search-content{
+      position: absolute;
+      top: 97rpx;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #fff;
+    }
+}
+	
 } 	
 
 </style>
